80663856e8b122a1a1950a3ec10d1ed42542b9be
bruno <brunomoraes.dev@gmail.com>
2024-12-07 19:17:46 -0300

    update

---
+++ engine.js
@@ -0,0 +1,26 @@
+const contadorElemento = document.getElementById('contador');
+const iniciarBotao = document.getElementById('iniciar');
+const pararBotao = document.getElementById('parar');
+const zerarBotao = document.getElementById('zerar');
+const voltaBotao = document.getElementById('volta');
+
+function iniciarCronometro() {
+  console.log('iniciar');
+}
+function pausarCronometro () {
+  console.log('pausar');
+}
+function pararCronometro() {
+  console.log('parar');
+}
+function zerarCronometro () {
+  console.log('zerar');
+}
+function registrarVolta () {
+  console.log('volta');
+}
+
+iniciarBotao.addEventListener('click', iniciarCronometro);
+pararBotao.addEventListener('click', pararCronometro);
+zerarBotao.addEventListener('click', zerarCronometro);
+voltaBotao.addEventListener('click', registrarVolta);
