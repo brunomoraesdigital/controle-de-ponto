f6310fbc9289a040664ae4a05e7b6cb0b8f157e0
Bruno Moraes <brunomoraes.dev@gmail.com>
2024-12-08 16:03:55 -0300

    081224.1603

--- engine.js
+++ engine.js
@@ -1,5 +1,7 @@
 const visorContadorElemento = document.getElementById('visorContador');
 const visorVoltaElemento = document.getElementById('visorVolta');
+const playIconeElemento = document.getElementById('playIcone');
+const pauseIconeElemento = document.getElementById('pauseIcone');
 
 const iniciarBotao = document.getElementById('iniciar');
 const pararBotao = document.getElementById('parar');
@@ -8,27 +10,62 @@ const voltaBotao = document.getElementById('volta');
 
 let numerosDoContador = 0;
 let intervalo= 0;
+let cronometroAtivo = false;
+let cronometroPausado = false;
+let cronometroParado = true;
 
 
 function iniciarCronometro() {
-  console.log('iniciar');
-  intervalo = setInterval(function () {
-    numerosDoContador++;
-    visorContadorElemento.textContent = numerosDoContador;
-  }, 1000);
-}
-function pausarCronometro () {
-  console.log('pausar');
+  if (cronometroAtivo == false && cronometroPausado == false) {
+    console.log('iniciar pressionado');
+    cronometroAtivo = true;
+    // true - false
+    intervalo = setInterval(function () {
+      numerosDoContador++;
+      visorContadorElemento.textContent = numerosDoContador;
+    }, 1000);
+    playIconeElemento.classList.toggle("esconder");
+    pauseIconeElemento.classList.toggle("esconder");
+  } else if (cronometroAtivo == true && cronometroPausado == false) {
+    console.log('pausar pressionado');
+    cronometroAtivo = false;
+    cronometroPausado = true;
+    playIconeElemento.classList.toggle("esconder");
+    pauseIconeElemento.classList.toggle("esconder");
+    // false - true
+    clearInterval(intervalo);
+  } else if (cronometroAtivo == false && cronometroPausado == true) {
+    console.log('retomar pressionado');
+    cronometroPausado = false;
+    // false - false
+    iniciarCronometro();
+  }
 }
 function pararCronometro() {
-  console.log('parar');
+if (cronometroAtivo == true || cronometroParado == true) {
+  console.log('parar pressionado');
+  cronometroAtivo = false;
+  cronometroPausado = false;
+  // false - false
   clearInterval(intervalo);
+  numerosDoContador = 0;
+  playIconeElemento.classList.remove('esconder');
+  pauseIconeElemento.classList.add('esconder');
+}
 }
 function zerarCronometro () {
-  console.log('zerar');
+  console.log('zerar pressionado');
+  cronometroAtivo = false;
+  cronometroPausado = false;
+  // false - false
+  clearInterval(intervalo);
+  numerosDoContador = 0;
+  visorContadorElemento.textContent = numerosDoContador;
+  playIconeElemento.classList.remove('esconder');
+  pauseIconeElemento.classList.add('esconder');
 }
 function registrarVolta () {
-  console.log('volta');
+  console.log('volta pressionado');
 }
 
 iniciarBotao.addEventListener('click', iniciarCronometro);
