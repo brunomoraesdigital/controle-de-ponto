8bb7699e24aa556fb381ffe07c96b0839f826d60
Bruno Moraes <brunomoraes.dev@gmail.com>
2024-12-08 19:03:59 -0300

    081224.1903

--- engine.js
+++ engine.js
@@ -9,14 +9,22 @@ const zerarBotao = document.getElementById('zerar');
 const voltaBotao = document.getElementById('volta');
 
 let numerosDoContador = 0;
-let intervalo= 0;
+let intervalo = 0;
 let cronometroAtivo = false;
 let cronometroPausado = false;
 let cronometroParado = true;
+let guardarVolta = [];
+let indiceDasVoltas = 0
 
 
 function iniciarCronometro() {
-  if (cronometroAtivo == false && cronometroPausado == false) {
+  if (cronometroAtivo === false && cronometroPausado === false) {
+    if (cronometroParado === true) {
+      visorContadorElemento.textContent = numerosDoContador;
+      cronometroParado = false;
+      visorVoltaElemento.textContent = '';
+      visorVoltaElemento.classList.add('esconder');
+    }
     console.log('iniciar pressionado');
     cronometroAtivo = true;
     // true - false
@@ -26,7 +34,7 @@ function iniciarCronometro() {
     }, 1000);
     playIconeElemento.classList.toggle("esconder");
     pauseIconeElemento.classList.toggle("esconder");
-  } else if (cronometroAtivo == true && cronometroPausado == false) {
+  } else if (cronometroAtivo === true && cronometroPausado === false) {
     console.log('pausar pressionado');
     cronometroAtivo = false;
     cronometroPausado = true;
@@ -34,7 +42,7 @@ function iniciarCronometro() {
     pauseIconeElemento.classList.toggle("esconder");
     // false - true
     clearInterval(intervalo);
-  } else if (cronometroAtivo == false && cronometroPausado == true) {
+  } else if (cronometroAtivo === false && cronometroPausado === true) {
     console.log('retomar pressionado');
     cronometroPausado = false;
     // false - false
@@ -42,31 +50,74 @@ function iniciarCronometro() {
   }
 }
 function pararCronometro() {
-if (cronometroAtivo == true || cronometroParado == true) {
-  console.log('parar pressionado');
-  cronometroAtivo = false;
-  cronometroPausado = false;
-  // false - false
-  clearInterval(intervalo);
-  numerosDoContador = 0;
-  playIconeElemento.classList.remove('esconder');
-  pauseIconeElemento.classList.add('esconder');
+  if ((cronometroAtivo === true && cronometroPausado === false) || (cronometroAtivo === false && cronometroPausado === true)) {
+    console.log('parar pressionado');
+    cronometroAtivo = false;
+    cronometroPausado = false;
+    cronometroParado = true;
+    guardarVolta = [];
+    indiceDasVoltas = 0;
+    // false - false
+    clearInterval(intervalo);
+    numerosDoContador = 0;
+    playIconeElemento.classList.remove('esconder');
+    pauseIconeElemento.classList.add('esconder');
+  }
 }
+function zerarCronometro() {
+  if ((cronometroAtivo === true && cronometroPausado === false) || (cronometroAtivo === false && cronometroPausado === true)) {
+    console.log('zerar pressionado');
+    cronometroAtivo = false;
+    cronometroPausado = false;
+    guardarVolta = [];
+    indiceDasVoltas = 0;
+    visorVoltaElemento.textContent = '';
+    visorVoltaElemento.classList.add('esconder');
+    // false - false
+    clearInterval(intervalo);
+    numerosDoContador = 0;
+    visorContadorElemento.textContent = numerosDoContador;
+    playIconeElemento.classList.remove('esconder');
+    pauseIconeElemento.classList.add('esconder');
+  }
 }
-function zerarCronometro () {
-  console.log('zerar pressionado');
-  cronometroAtivo = false;
-  cronometroPausado = false;
-  // false - false
-  clearInterval(intervalo);
-  numerosDoContador = 0;
-  visorContadorElemento.textContent = numerosDoContador;
-  playIconeElemento.classList.remove('esconder');
-  pauseIconeElemento.classList.add('esconder');
+function registrarVolta() {
+
+  visorVoltaElemento.classList.remove('esconder');
+
+  if (cronometroAtivo === true && cronometroPausado === false) {
+    console.log('volta pressionado');
+
+    let criarRegistro = false;
+
+    if (guardarVolta.length < 20) {
+      guardarVolta.push(numerosDoContador);
+      criarRegistro = true;
+    }
+    else if (guardarVolta.length === 20) {
+      guardarVolta.push('Atingiu limite!');
+      criarRegistro = true;
+    }
+    else {
+      criarRegistro = false;
+    }
+
+    if (criarRegistro === true) {
+      const tagP = document.createElement('p');
+      if (guardarVolta.length < 21) {
+        tagP.textContent = `Volta-${indiceDasVoltas + 1}: ${guardarVolta[indiceDasVoltas]}`;
+
+      } else {
+        tagP.textContent = `${guardarVolta[indiceDasVoltas]}`;
+      }
+
+      visorVoltaElemento.appendChild(tagP);
+      indiceDasVoltas++;
+
+    }
+  }
 }
-function registrarVolta () {
-  console.log('volta pressionado');
-}
+
 
 iniciarBotao.addEventListener('click', iniciarCronometro);
 pararBotao.addEventListener('click', pararCronometro);
