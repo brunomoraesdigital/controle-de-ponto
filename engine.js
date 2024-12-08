19083c0b4883bc2b1aea2e2d5b2aef0ef32197e4
bruno <brunomoraes.dev@gmail.com>
2024-12-07 22:09:14 -0300

    update engine.js

--- engine.js
+++ engine.js
@@ -6,14 +6,23 @@ const pararBotao = document.getElementById('parar');
 const zerarBotao = document.getElementById('zerar');
 const voltaBotao = document.getElementById('volta');
 
+let numerosDoContador = 0;
+let intervalo= 0;
+
+
 function iniciarCronometro() {
   console.log('iniciar');
+  intervalo = setInterval(function () {
+    numerosDoContador++;
+    visorContadorElemento.textContent = numerosDoContador;
+  }, 1000);
 }
 function pausarCronometro () {
   console.log('pausar');
 }
 function pararCronometro() {
   console.log('parar');
+  clearInterval(intervalo);
 }
 function zerarCronometro () {
   console.log('zerar');
